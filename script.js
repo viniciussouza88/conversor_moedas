241ccbad050ac236e13c1d0e616adcbc346bc5fe
viniciussouza88 <souza.vinicius.88@gmail.com>
2024-04-16 00:27:26 -0300

    Alterando valor das moedas a converter e convertida - innerHTML e Intl.NumberFormat

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const convertButton = document.querySelector(".convert-button")
 function convertValues() {
     
     const inputValue = document.querySelector(".input-value").value
+    const currencyValuetoConvert = document.querySelector(".currency-value-to-convert") //valor a converter
+    const currencyValueConverted = document.querySelector(".currency-value-converted") //valor convertido para outras moedas
+    
+    const dolar = 5.10
 
-    const dolar = 5.40
+    const convertedValue = inputValue / dolar
 
-    const convertedValue = inputValue * dolar
+    currencyValuetoConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputValue)
 
-    console.log(convertedValue)
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
+    
 }
 
 convertButton.addEventListener("click", convertValues)
